489f24024597a5995dbf815e38cecc21625148dc
LenoMac <129600379+LenoMac@users.noreply.github.com>
2023-05-26 11:49:38 +0600

    upgrated

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const value1 = document.querySelector('.add-task input'),
         btn = document.querySelector('.add-task button'),
         list = document.querySelector('.list'),
-        img = document.createElement('nav'),
-        div1 = document.createElement('div')
+        left = document.querySelector('.left')
 
     value1.addEventListener('input', () => {
         if (value1.value) {
             btn.style.background = 'blueviolet'
             btn.style.cursor = 'pointer'
+
+            setTimeout(() => {
+                left.style.cssText = 'opacity: 1; width: 300px;'
+            }, 1000)
         } else {
             btn.style.background = 'rgb(181, 101, 255)'
             btn.style.cursor = 'no-drop'
+            setTimeout(() => {
+                left.style.cssText = 'opacity: 0; width: 10px;'
+            }, 500)
         }
     })
     btn.onclick = () => {
@@ -27,19 +33,15 @@ document.addEventListener("DOMContentLoaded", () => {
             list.style.cssText = 'display: flex; flex-direction: column; gap: 1rem;'
             let li = document.createElement('li'),
                 div = document.createElement('div'),
-                p = document.createElement('p')
-
-
+                p = document.createElement('p'),
+                img = document.createElement('nav')
             span = document.createElement('span')
             div.style.cssText = 'display: flex; gap: 7px;'
-            div1.style.cssText = 'align-self: center;'
             img.style.cssText = 'min-width: 20px; min-height: 20px; align-self: center; border-radius: 50%; border: 1px solid gray;'
             li.style.cssText = 'display: flex; width: 100%; justify-content: space-between;'
             p.innerHTML = text
 
-
-            div1.insertAdjacentElement("afterbegin", img)
-            div.insertAdjacentElement("afterbegin", div1)
+            div.insertAdjacentElement("afterbegin", img)
             div.insertAdjacentElement("beforeend", p)
             li.insertAdjacentElement("afterbegin", div)
             span.setAttribute('class', 'material-symbols-outlined delete')
@@ -51,17 +53,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-
     list.addEventListener('click', function (e) {
         if (e.target.tagName === "P") {
             e.target.classList.toggle("checked")
-
             if (e.target.classList.contains("checked")) {
-                div1.innerHTML = `<img src="./img/todo.png" style="width: 20px; align-self:center;" />`
-                saveTasks()
+                e.target.parentElement.classList.add('done')
             } else {
-                div1.innerHTML = `<nav style="min-width: 20px; min-height: 20px; align-self: center; border-radius: 50%; border: 1px solid gray;"></nav>`
-                saveTasks()
+                e.target.parentElement.classList.remove('done')
             }
             saveTasks()
         }
@@ -70,12 +68,26 @@ document.addEventListener("DOMContentLoaded", () => {
             saveTasks()
         }
 
-        let liElements = list.getElementsByTagName('li')
-        if (liElements.length === 0) {
-            list.style.display = 'none'
-            saveTasks()
-        }
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     function saveTasks() {
         localStorage.setItem("data", list.innerHTML)
         localStorage.setItem("style", list.style.cssText)
@@ -85,7 +97,6 @@ document.addEventListener("DOMContentLoaded", () => {
         list.innerHTML = localStorage.getItem("data")
         list.style.cssText = localStorage.getItem("style")
     }
-
     showTasks()
 
 })
